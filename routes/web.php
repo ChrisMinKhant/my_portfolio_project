15f04a1954a6a961fcc2dc1f9cb4b26802e85fe3
Kaung Min Khant <kaungminkhant05723123@gmail.com>
2022-11-05 10:05:58 +0630

    4th-commit for my_portfolio_project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,20 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Initial Route
+Route::get('/',function(){
+    return view('user.home');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// Routes for user
+Route::prefix('user')->controller(UserController::class)->group(function(){
+    Route::get('home','userHomePage')->name('user#homepage');
+    Route::get('about','userAboutPage')->name('user#aboutpage');
+    Route::get('projects','userProjectsPage')->name('user#projectspage');
+    Route::get('contact','userContactPage')->name('user#contactpage');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
