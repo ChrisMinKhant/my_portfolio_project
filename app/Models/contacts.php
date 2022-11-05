99f5d495aae7ce361eb96c9c951a7578f5b4265e
Kaung Min Khant <kaungminkhant05723123@gmail.com>
2022-11-05 07:27:58 +0630

    3rd-commit for my_portfolio_project

--- app/Models/contacts.php
+++ app/Models/contacts.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class contacts extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'message',
+    ];
 }
--- database/migrations/2022_11_05_000534_create_contacts_table.php
+++ database/migrations/2022_11_05_000534_create_contacts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->char('phone',15);
+            $table->string('message');
             $table->timestamps();
         });
     }
