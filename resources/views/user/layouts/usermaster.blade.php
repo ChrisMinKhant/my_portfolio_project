4a8f3635a9931f871bfa834223c87843712df43e
Kaung Min Khant <kaungminkhant05723123@gmail.com>
2022-11-08 17:49:39 +0630

    5th-commit for my_portfolio_project

--- resources/views/user/layouts/usermaster.blade.php
+++ resources/views/user/layouts/usermaster.blade.php
@@ -10,13 +10,18 @@
     {{-- Bootstrap --}}
     @vite(['resources/js/app.js'])
 
+    {{-- Fontawsome Link --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
     <div class="container-fluid">
         {{-- Nav Bar Open --}}
-        <div class="row">
-            <nav class="navbar navbar-expand-lg bg-light">
+        <div class="row shadow-sm">
+            <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid align-content-center">
                     <span class="navbar-brand text-capitalize fs-1" href="#">kaung min khant</span>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -26,7 +31,8 @@
                     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="{{ route('user#homepage') }}">Home</a>
+                                <a class="nav-link active" aria-current="page"
+                                    href="{{ route('user#homepage') }}">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('user#aboutpage') }}">About Me</a>
@@ -46,7 +52,7 @@
 
         {{-- Main Content Open --}}
 
-            @yield('main_content')
+        @yield('main_content')
 
         {{-- Main Content Close --}}
     </div>
