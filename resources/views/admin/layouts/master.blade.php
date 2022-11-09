3386fbdea89907f185d16db9e7c53f23ca689dd1
Kaung Min Khant <kaungminkhant05723123@gmail.com>
2022-11-09 17:14:53 +0630

    6th-commit for my_portfolio_project

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    {{-- Bootstrap --}}
+    @vite(['resources/js/app.js'])
+
+    {{-- Fontawsome Link --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        {{-- Header Open --}}
+        <div class="row shadow-sm text-center">
+            <p class="fs-1 fw-bold text-uppercase mt-3">Kaung Min Khant - <span class="text-primary">Admin
+                    Dashboard</span></p>
+        </div>
+        {{-- Header Close --}}
+        {{-- Nav Bar Open --}}
+        <div class="row bg-dark" style="height: 100vh;">
+            {{-- Nav Bar Open --}}
+            <div class="col-2 mt-3">
+                <nav class="nav flex-column px-4">
+                    <a class="btn btn-light text-dark rounded-3 position-relative my-5" href="{{ route('admin#messagepannel') }}">
+                        <i class="fa-solid fa-inbox me-2"></i>Inbox
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            @yield('inboxCount')
+                        </span>
+                    </a>
+                    <a class="nav-link text-decoration-none btn btn-light text-dark rounded-3" href="#"><i
+                            class="fa-solid fa-arrow-up-from-bracket me-2"></i>Project Upload</a>
+                </nav>
+            </div>
+            {{-- Nav Bar Close --}}
+            {{-- Content Open --}}
+            @yield('main_content')
+            {{-- Content Close --}}
+        </div>
+        {{-- Nav Bar Close --}}
+    </div>
+</body>
+@yield('script_session')
+
+</html>
---
+++ resources/views/admin/message.blade.php
@@ -0,0 +1,43 @@
+@extends('admin.layouts.master')
+
+@section('title')
+    Kaung Min Khant - Admin Page
+@endsection
+
+@section('inboxCount')
+    @if ($contactData->count() >= 100)
+        +99
+    @endif
+
+    {{ $contactData->count() }}
+@endsection
+
+@section('main_content')
+    <div class="col-10 mt-5">
+        <table class="table text-light">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Message</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($contactData as $contacts)
+                    <tr>
+                        <td scope="row">{{ $contacts->created_at->format('M-d-Y') }}</td>
+                        <td>{{ $contacts->name }}</td>
+                        <td>{{ $contacts->email }}</td>
+                        <td>{{ $contacts->phone }}</td>
+                        <td>{{ Str::limit($contacts->message, 20, '...') }}</td>
+                        <td><a href="{{ route('admin#deletemessage', $contacts->id) }}"
+                                class="text-decoration-none text-danger"><i class="fa-solid fa-trash-can"></i></a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
